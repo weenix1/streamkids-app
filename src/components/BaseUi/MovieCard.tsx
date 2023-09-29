77b6515a05da6caa3ded571ebfd8be0a702e5929
Hilary <hilary.ogalagu@aldautomotive.com>
2023-09-29 10:27:42 +0200

    added width and height to moviecard

--- src/components/BaseUi/MovieCard.tsx
+++ src/components/BaseUi/MovieCard.tsx
@@ -5,18 +5,42 @@ import {
 } from "@material-tailwind/react";
 import Text from "./Text";
 import MoreIcon from "../../assets/icons/more_vert.svg";
-import { ConfigPosition } from "../../types";
+import { ConfigPosition, ConfigSize } from "../../types";
 import Box from "./Box";
 import ChipPill from "./ChipPill";
 
-interface CardWithFooterProps {
+interface MovieCardProps {
   duration?: string;
   title?: string;
   iconPosition: ConfigPosition;
   isFooter?: boolean;
+  isPlaying?: boolean;
+  height?: ConfigSize;
+  width?: ConfigSize;
 }
 
-const CardWithFooter = ({ duration, title, iconPosition, isFooter = false }: CardWithFooterProps) => {
+const widthSizeValues: Record<ConfigSize, string> = {
+  xs: 'w-40',
+  sm: 'w-27',
+  md: 'w-80',
+  lg: 'w-760',
+  xl: 'w-full',
+  '2xl': 'w-screen',
+};
+
+const heightSizeValues: Record<ConfigSize, string> = {
+  xs: 'h-40',
+  sm: 'h-60',
+  md: 'h-80',
+  lg: 'h-760',
+  xl: 'h-full',
+  '2xl': 'h-screen',
+};
+
+const MovieCard = ({ duration, title, iconPosition, isFooter = false, isPlaying = false, height = 'md', width = 'sm' }: MovieCardProps) => {
+
+  const widthValue = width ? widthSizeValues[width] : '';
+  const heightValue = height ? heightSizeValues[height] : '';
 
   const alignmentStyles = {
     start: {
@@ -29,7 +53,7 @@ const CardWithFooter = ({ duration, title, iconPosition, isFooter = false }: Car
     },
     end: {
       icon: 'absolute top-1/2 right-2 transform -translate-y-1/2',
-      text: 'absolute top-1/2 left-2 transform -translate-y-1/2',
+      text: 'absolute bottom-2 right-2 transform -translate-y-1/2',
     },
     top: {
       icon: 'absolute top-2 left-2 transform -translate-y-1/2',
@@ -41,7 +65,7 @@ const CardWithFooter = ({ duration, title, iconPosition, isFooter = false }: Car
     },
     bottom: {
       icon: 'absolute bottom-2 left-2 transform -translate-y-1/2',
-      text: 'absolute bottom-1/2 left-2 transform -translate-y-1/2',
+      text: 'absolute bottom-2 left-2 transform -translate-y-1/2',
     },
     left: {
       icon: 'absolute top-1/2 left-2 transform -translate-y-1/2',
@@ -52,7 +76,7 @@ const CardWithFooter = ({ duration, title, iconPosition, isFooter = false }: Car
   const selectedStyle = alignmentStyles[iconPosition] || alignmentStyles.end; // default to 'left' if no valid alignment provided
 
   return (
-    <Card className="max-w-[24rem] overflow-hidden">
+    <Card className={`${widthValue} ${heightValue} overflow-hidden `}>
       <CardHeader
         floated={false}
         shadow={false}
@@ -75,4 +99,4 @@ const CardWithFooter = ({ duration, title, iconPosition, isFooter = false }: Car
   );
 }
 
-export default CardWithFooter;
+export default MovieCard;
--- src/components/MovieCardStory.tsx
+++ src/components/MovieCardStory.tsx
@@ -1,5 +1,5 @@
 import LayoutPanel from "./BaseUi/LayoutPanel";
-import CardWithFooter from "./BaseUi/CardWithFooter";
+import MovieCard from "./BaseUi/MovieCard";
 import { CaseGroup } from "./BaseUi/CaseGroup";
 import { Case } from "./BaseUi/Case";
 
@@ -9,48 +9,48 @@ const MovieCardStory = () => {
     <LayoutPanel>
       <CaseGroup title='text position with footer'>
         <Case title='Top'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" isFooter iconPosition='top' />
+          <MovieCard width="sm" height="md" title='Power rangers' duration="1h 20min" isFooter iconPosition='top' />
         </Case>
         <Case title='Bottom'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" isFooter iconPosition='bottom' />
+          <MovieCard width="sm" height="md" title='Power rangers' duration="1h 20min" isFooter iconPosition='bottom' />
         </Case>
         <Case title='Left'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" isFooter iconPosition='left' />
+          <MovieCard width="sm" height="md" title='Power rangers' duration="1h 20min" isFooter iconPosition='left' />
         </Case>
         <Case title='Right'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" isFooter iconPosition='right' />
+          <MovieCard width="sm" height="md" title='Power rangers' duration="1h 20min" isFooter iconPosition='right' />
         </Case>
         <Case title='Center'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" isFooter iconPosition='center' />
+          <MovieCard width="sm" height="md" title='Power rangers' duration="1h 20min" isFooter iconPosition='center' />
         </Case>
         <Case title='Start'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" isFooter iconPosition='start' />
+          <MovieCard width="sm" height="md" title='Power rangers' duration="1h 20min" isFooter iconPosition='start' />
         </Case>
         <Case title='End'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" isFooter iconPosition='end' />
+          <MovieCard width="sm" height="md" title='Power rangers' duration="1h 20min" isFooter iconPosition='end' />
         </Case>
       </CaseGroup>
       <CaseGroup title='text position without footer'>
         <Case title='Top'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" iconPosition='top' />
+          <MovieCard width="sm" height="sm" title='Power rangers' duration="1h 20min" iconPosition='top' />
         </Case>
         <Case title='Bottom'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" iconPosition='bottom' />
+          <MovieCard width="sm" height="sm" title='Power rangers' duration="1h 20min" iconPosition='bottom' />
         </Case>
         <Case title='Left'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" iconPosition='left' />
+          <MovieCard width="sm" height="sm" title='Power rangers' duration="1h 20min" iconPosition='left' />
         </Case>
         <Case title='Right'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" iconPosition='right' />
+          <MovieCard width="sm" height="sm" title='Power rangers' duration="1h 20min" iconPosition='right' />
         </Case>
         <Case title='Center'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" iconPosition='center' />
+          <MovieCard width="sm" height="sm" title='Power rangers' duration="1h 20min" iconPosition='center' />
         </Case>
         <Case title='Start'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" iconPosition='start' />
+          <MovieCard width="sm" height="sm" title='Power rangers' duration="1h 20min" iconPosition='start' />
         </Case>
         <Case title='End'>
-          <CardWithFooter title='Power rangers' duration="1h 20min" iconPosition='end' />
+          <MovieCard width="sm" height="sm" title='Power rangers' duration="1h 20min" iconPosition='end' />
         </Case>
       </CaseGroup>
     </LayoutPanel>
